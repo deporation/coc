5e01614950229452da759a0ef466be1cd53ab56d
deporation <strikebiood@163.com>
2021-01-03 17:34:50 +0800

    用户登录功能，add common公共模块，添加md5验证

--- user/src/main/java/com/tisu/user/controller/UserController.java
+++ user/src/main/java/com/tisu/user/controller/UserController.java
@@ -2,16 +2,24 @@ package com.tisu.user.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
+import com.google.common.net.MediaType;
+import com.tisu.commons.entity.ResponseResult;
 import com.tisu.user.entity.User;
 import com.tisu.user.service.UserService;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,22 +32,25 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    /**
-     * 在MyRedisConfig文件中配置了redisTemplate的序列化之后， 客户端也能正确显示键值对了
-     */
-    @Autowired
-    private RedisTemplate<String,Object> redisTemplate;
 
     @RequestMapping(value = "/login",method = {RequestMethod.POST})
-    @ApiOperation("根据id查询用户")
-    @ApiImplicitParam(name = "id",value = "用户id",required = true)
-    public String login(@RequestBody Map<String,String> query){
-        User user = (User) redisTemplate.opsForValue().get("user"+query.get("id"));
-         if (user == null){
-            user = userService.findUserById(Integer.parseInt(query.get("id")));
-            redisTemplate.opsForValue().set("user"+query.get("id"),user,30, TimeUnit.SECONDS);
+    @ApiOperation("用户登录")
+    public String login(@RequestBody User user, HttpServletRequest request) throws Exception {
+        User search = userService.login(user);
+        ResponseResult responseResult = new ResponseResult();
+         if (search != null){
+             HttpSession session = request.getSession();
+             String accessToken = UUID.randomUUID().toString();
+             session.setAttribute("accessToken", accessToken);
+             session.setAttribute("user_id",user.getId());
+             Map<String,Object> map = new HashMap<>();
+             map.put("access_token",accessToken);
+             map.put("userId",search.getId());
+             responseResult = ResponseResult.builder().success(true).state(200).message("success").content(map).build();
+         }else{
+             responseResult = ResponseResult.builder().success(false).state(400).message("failure").content(null).build();
          }
-        return JSON.toJSONString(user);
+        return JSON.toJSONString(responseResult);
     }
 
 }
