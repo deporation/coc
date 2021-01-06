f7cf6513ff03de0d30ab8b28e133ab00c84ef198
sunyufan <deporation1@gmail.com>
2021-01-06 13:45:13 +0800

    gateway添加跨域信息配置

---
+++ gateway/src/main/java/com/tisu/gateway/cors/AjaxCorsFilter.java
@@ -0,0 +1,44 @@
+package com.tisu.gateway.cors;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <p>跨域信息配置</p>
+ *
+ * @author deporation
+ * @date 2021/1/6
+ **/
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class AjaxCorsFilter extends CorsFilter {
+    public AjaxCorsFilter() {
+        super(configurationSource());
+    }
+
+    private static CorsConfigurationSource configurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        List<String> allowedHeaders = Arrays.asList("X-Auth-Token", "Content-Type", "X-Requested-With", "XMLHttpRequest");
+        List<String> exposedHeaders = Arrays.asList("X-Auth-Token", "Content-Type", "X-Requested-With", "XMLHttpRequest");
+        List<String> allowedMethods = Arrays.asList("POST", "GET", "DELETE", "PUT", "OPTIONS");
+        List<String> allowedOrigins = Arrays.asList("*");
+        corsConfig.setAllowedHeaders(allowedHeaders);
+        corsConfig.setAllowedMethods(allowedMethods);
+        corsConfig.setAllowedOrigins(allowedOrigins);
+        corsConfig.setExposedHeaders(exposedHeaders);
+        corsConfig.setMaxAge(36000L);
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+}
